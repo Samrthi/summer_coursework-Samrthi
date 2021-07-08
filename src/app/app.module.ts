d8f7745699c754e0ddf496290ff1c396b4effa9d
Sam Thiesen <Sam.R.Thiesen@gmail.com>
2021-07-08 13:27:12 +0100

    create angular components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CandidateProfileComponent } from './candidate-profile/candidate-profile.component';
+import { EmployerProfileComponent } from './employer-profile/employer-profile.component';
+import { JobListingComponent } from './job-listing/job-listing.component';
+import { LoginComponent } from './login/login.component';
+import { CandidateListingComponent } from './candidate-listing/candidate-listing.component';
+import { JobEditorComponent } from './job-editor/job-editor.component';
+import { CandidateListComponent } from './candidate-list/candidate-list.component';
+import { JobListComponent } from './job-list/job-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CandidateProfileComponent,
+    EmployerProfileComponent,
+    JobListingComponent,
+    LoginComponent,
+    CandidateListingComponent,
+    JobEditorComponent,
+    CandidateListComponent,
+    JobListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/job-editor/job-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-job-editor',
+  templateUrl: './job-editor.component.html',
+  styleUrls: ['./job-editor.component.scss']
+})
+export class JobEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
