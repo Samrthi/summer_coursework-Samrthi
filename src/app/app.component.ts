b5ba308cfa78a27a9e3bc5f6e7367cbde1837c14
Sam Thiesen <Sam.R.Thiesen@gmail.com>
2021-07-11 02:46:18 +0100

    add logout button to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {StorageService} from "./storage.service";
+import {CookieService} from "ngx-cookie-service";
+import {AuthService} from "./auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = "Robert's list";
+  logged_in = false
+
+  constructor(
+      private storage: StorageService,
+      private auth: AuthService,
+      private cookieService: CookieService ) {}
+
+  ngOnInit(): void {
+    if (this.cookieService.check('t')) {
+      this.logged_in = true
+    }
+  }
+
+
+  signOut(): void {
+    this.auth.logout()
+    // route to login page
+  }
 }
