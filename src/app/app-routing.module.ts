10061b959fa5d02fcf4fdbcd16eddac5ced2b80c
Sam Thiesen <Sam.R.Thiesen@gmail.com>
2021-07-11 02:46:44 +0100

    remove redundant authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {LoginComponent} from "./login/login.component";
 import {CandidateProfileComponent} from "./candidate-profile/candidate-profile.component";
-import {AuthGuard} from "./auth.guard";
 import {CandidateListComponent} from "./candidate-list/candidate-list.component";
 import {CandidateListingComponent} from "./candidate-listing/candidate-listing.component";
 import {Employer} from "./employer";
@@ -12,34 +11,12 @@ import {JobListingComponent} from "./job-listing/job-listing.component";
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  {
-    path: 'candidate-profile',
-    component: CandidateProfileComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'candidates',
-    component: CandidateListComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'candidate',
-    component: CandidateListingComponent,
-    canActivate: [AuthGuard]
-  },
-
-  {
-    path: 'employer-profile',
-    component: Employer,
-    canActivate: [AuthGuard]
-  },
-
-  {
-    path: 'job-listing',
-    component: JobListingComponent,
-    canActivate: [AuthGuard]
-  },
-  { path: 'unauthenticated', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'candidate-profile',component: CandidateProfileComponent },
+  { path: 'candidates',component: CandidateListComponent },
+  { path: 'candidate',component: CandidateListingComponent },
+  { path: 'employer-profile',component: Employer },
+  { path: 'job-listing',component: JobListingComponent },
+  { path: 'unauthenticated', redirectTo: '/login', pathMatch: 'full' }, // needed?
 ];
 
 
