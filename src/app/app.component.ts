010f97eda6c158d75c45cfaffade3e10e9e2f2e4
Sam Thiesen <Sam.R.Thiesen@gmail.com>
2021-07-20 22:29:01 +0100

    add notification service to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {StorageService} from "./storage.service";
 import {AuthService} from "./auth.service";
+import {NotificationService} from "./notification.service";
+import {MatDialog} from "@angular/material/dialog";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-root',
@@ -11,10 +13,32 @@ export class AppComponent {
   title = "Robert's list";
 
   constructor(
-      public auth: AuthService
-  ){};
+      public auth: AuthService,
+      private notificationService: NotificationService,
+      private snackBar: MatSnackBar,
+      private dialog: MatDialog
+  ) {
+    this.notificationService.notification$.subscribe((message) => {
+      this.snackBar.open(message, '', { duration: 2000 });
+    });
+    this.notificationService.error$.subscribe((message) => {
+      this.dialog.open(PopupComponent, { data: message });
+    });
+  }
 
   signOut(): void {
     this.auth.logout()
   }
+}
+
+
+import { Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './popup.html',
+})
+export class PopupComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: string) {}
 }
