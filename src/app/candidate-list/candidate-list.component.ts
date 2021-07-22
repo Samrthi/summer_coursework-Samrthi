65b60b0d407d5f706bc8c16bdee46eb779cff082
Sam Thiesen <Sam.R.Thiesen@gmail.com>
2021-07-22 21:36:54 +0100

    Style candidate list

--- src/app/candidate-list/candidate-list.component.ts
+++ src/app/candidate-list/candidate-list.component.ts
@@ -6,33 +6,34 @@ import {of} from "rxjs";
 import {Job} from "../job";
 
 @Component({
-  selector: 'app-candidate-list',
-  templateUrl: './candidate-list.component.html',
-  styleUrls: ['./candidate-list.component.scss']
+    selector: 'app-candidate-list',
+    templateUrl: './candidate-list.component.html',
+    styleUrls: ['./candidate-list.component.scss']
 })
 export class CandidateListComponent implements OnInit {
-  candidates?: Candidate[];
+    candidates?: Candidate[];
+    skillDict = {};
 
 
-  constructor(private storage: StorageService,) { }
+    constructor(private storage: StorageService,) { }
 
-  ngOnInit(): void {
-    this.loadData()
-  }
-
-  private loadData(): void {
-    this.storage.getSearchableCandidates()
-        .pipe(
-            catchError((err) => {
-              console.log(err)
-              return of([], false)
-            }))
-        // @ts-ignore
-        .subscribe((candidates?: Candidate[]) => {
-          if (candidates) {
-            this.candidates = candidates
-          }
+    ngOnInit(): void {
+        this.storage.getSkillList().subscribe(skills => {
+            skills.forEach(skill => {
+                this.skillDict[skill._id] = skill.name
+            })
         })
-  }
 
+        this.storage.getSearchableCandidates().subscribe((candidates: Candidate[]) => {
+            this.candidates = this.convertSkillIdtoName(candidates)
+        })
+    }
+
+
+    convertSkillIdtoName(candidates: Candidate[]) {
+        return candidates.map(candidate => {
+            candidate.skills = candidate.skills.map(skill => this.skillDict[skill])
+            return candidate
+        })
+    }
 }
